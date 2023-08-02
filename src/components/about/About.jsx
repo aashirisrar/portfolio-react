7f4ed364434c5f49bd51478906992512a0477449
Aashir Israr <63807168+aashirisrar@users.noreply.github.com>
2023-08-03 02:16:34 +0500

    minor changes

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,6 +1,6 @@
 import "./about.css";
 import AboutImg from "../../assets/about.jpg";
-import CV from "../../assets/John-Cv.pdf";
+import CV from "../../assets/Aashir_Israr-Resume.pdf";
 import Info from "./Info";
 
 const About = () => {
